98e927ce07571408e927ff4172488e97bbdf4d60
IrieY56 <IrieY56@gmail.com>
2017-10-01 16:59:20 -0400

    Added express framework and mongoDB functionality to the website. New dependencies not included is express, mongoose, body-parser, and of course mongoDB.

---
+++ con-cat/app.js
@@ -0,0 +1,48 @@
+var express    = require("express"),
+    app        = express(),
+    mongoose   = require("mongoose"),
+    bodyParser = require("body-parser");
+    
+mongoose.connect("mongodb://localhost/con_cat_test");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+//SCHEMA Setup
+var testdataSchema = new mongoose.Schema({
+    name: String,
+    //category: Boolean
+});
+
+var Testdata = mongoose.model("Testdata", testdataSchema);
+
+app.get("/", function(req, res){
+    //Get all data from DB
+    Testdata.find({}, function(err, allData){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("landing", {testData: allData});
+        }
+    });
+});
+
+app.get("/new", function(req, res){
+    res.render("new");
+});
+
+app.post("/", function(req, res){
+    var bodyData = req.body.testdata;
+    var testData = {name: bodyData};
+    
+    Testdata.create(testData, function(err, newItem){
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    })
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started!");
+})
