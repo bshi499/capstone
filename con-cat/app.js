41d0fe28d9f6627045a57a76d9cb7b9aedcfd04a
IrieY56 <IrieY56@gmail.com>
2017-10-02 16:22:32 -0400

    updated to have Michael's changes on Oct 1

--- con-cat/app.js
+++ con-cat/app.js
@@ -1,16 +1,11 @@
-//Initial setup for dependencies
 var express    = require("express"),
     app        = express(),
     mongoose   = require("mongoose"),
     bodyParser = require("body-parser");
-
-//Connect to mongoDB database
+    
 mongoose.connect("mongodb://localhost/con_cat_test");
-//More setup body-parser
 app.use(bodyParser.urlencoded({extended: true}));
-//Tell express to look in the /public file path
-app.use(express.static("public"));
-//Tell express to assume all files are .ejs at default
+//app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 //SCHEMA Setup
@@ -32,16 +27,14 @@ app.get("/", function(req, res){
     });
 });
 
-//render /views/new.ejs
 app.get("/new", function(req, res){
     res.render("new");
 });
 
-//Add new data to landing page
 app.post("/", function(req, res){
     var bodyData = req.body.testdata;
     var testData = {name: bodyData};
-
+    
     Testdata.create(testData, function(err, newItem){
         if(err) {
             console.log(err);
@@ -51,7 +44,6 @@ app.post("/", function(req, res){
     })
 })
 
-//Port to listen to. Requests C9's PORT and IP.
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!");
 })
