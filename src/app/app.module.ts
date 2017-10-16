06e19eda46615377e01738bb46561e54404a475b
mciminesi <michael.ciminesi78@myhunter.cuny.edu>
2017-10-15 21:30:48 -0400

    Adding heroku code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // Import for NgModel
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FileDetailComponent } from './file-detail.component'
+import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContactDetailsComponent,
+    ContactListComponent,
     FileDetailComponent
   ],
   imports: [
     BrowserModule,
     // Import FormsModule here so you can bind with ngModel command
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
