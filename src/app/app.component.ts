ed568a3f3859b70d47cef680065d27b3ab2400e7
mciminesi <michael.ciminesi78@myhunter.cuny.edu>
2017-11-01 22:06:11 -0400

    Spacing fix.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { DocumentService } from './documents/document.service';
-import { Document } from './documents/document';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [DocumentService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ConCat: Conceptualizer and Categorizer';
@@ -18,7 +15,7 @@ export class AppComponent {
       body: "body from AppComponent"
     };
 
-  constructor(private documentService: DocumentService) {
+  constructor() {
   }
 
   yell(e) {
