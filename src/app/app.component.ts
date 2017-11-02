a61740d7973acaa2b6f1d2244033016bd96c3951
mciminesi <michael.ciminesi78@myhunter.cuny.edu>
2017-11-01 20:30:19 -0400

    Moving service to root component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
+import { DocumentService } from '../document.service';
+import { Document } from '../document';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DocumnentService]
 })
 export class AppComponent {
   title = 'ConCat: Conceptualizer and Categorizer';
+
+
   name = 'file';
     file = {
       name: "name from AppComponent",
       body: "body from AppComponent"
     };
+
+  constructor(private documentService: DocumentService, private route: ActivatedRoute) {
+  }
+
   yell(e) {
     alert("I am yelling.");
     console.log(e);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document';
+import { DocumentService } from '../document.service';
+
 declare function escape(s:string): string;
 
 @Component({
@@ -8,6 +11,22 @@ declare function escape(s:string): string;
 })
 export class HomeComponent implements OnInit {
   homeTitle = "Welcome to the homepage (file upload)!";
+  description = 'This app can be used to cluster text documnets based on their content.';
+  instructions = 'Upload text files that you would like clusted to the database. Clustering can be run from the analysis page, where results will appear.';
+
+
+  @Input()
+  doc: Document;
+
+  @Input()
+  createHandler: Function;
+
+  createDocument(document: Document) {
+    this.documentService.createDocument(doc).then((newDocument: Document) => {
+      this.createHandler(newDocument);
+    });
+  }
+
   myString = "Testing";
   myBoolean = false;
 
@@ -28,7 +47,7 @@ export class HomeComponent implements OnInit {
 
   public fileString;
 
-  constructor() {
+  constructor(private documentService: DocumentService) {
     this.fileString;
   }
 
