4c9872b2c01f578c5b6f53e82735c9a748e179be
sli2147 <sl5946@gmail.com>
2017-11-21 17:47:33 -0500

    Added updateAll route

--- server.js
+++ server.js
@@ -126,12 +126,19 @@ app.delete("/api/documents/:id", function(req, res) {
       to be converted into vector
 */
 
+/*
+    /api/uploads/
+    - receives the file string from the upload component
+      to be converted into vector
+*/
+
 app.post("/api/uploads/", function(req, res) {
 
   var fileText = req.body.text;
   var finalOutput = '';
   
   // to convert input into array for testing
+  // remove later when needed
   var arr = [];
   var temp = '';
   var num = 0;
@@ -176,7 +183,58 @@ app.post("/api/uploads/", function(req, res) {
 });
 
 
+// for updating all unconverted documents
+// slightly modified from uploads
+app.post("/api/updateAll/", function(req, res) {
 
+  var fileText = req.body.text;
+  var finalOutput = '';
+  
+  // to convert input into array for testing
+  // remove later when needed
+  var arr = [];
+  var temp = '';
+  var num = 0;
+    
+  for(var i = 0; i <= fileText.length; i++)
+  {
+      if(fileText[i] != ' ' && i != fileText.length)
+      {
+        temp += fileText[i];
+      }
+      else
+      {
+        arr[num] = temp;
+        num++;
+        temp = '';
+      }
+  }
+  // end block
+
+
+  // python communication
+
+  var spawn = child_proc.spawn;
+  var py    = spawn('python', ['email2vector.py']);
+
+  py.stdout.on('data', function(data){
+    finalOutput += data.toString();
+  });
+
+  py.stdout.on('end', function(){
+    // console.log("Output ", finalOutput);
+    var returnBody = {text: finalOutput, idx: req.body.num};
+    res.send(circJSON.stringify(returnBody));
+  });
+
+  py.stdin.write(JSON.stringify(arr));
+  py.stdin.end();
+
+
+  py.on('exit', function (code, signal) {
+    console.log("child process exited with code " + code + " and signal " + signal);
+  });
+});
 
 
 // app.get('*', function(req, res){
