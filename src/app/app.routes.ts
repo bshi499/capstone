19aeabf7b80c743b48be3663eb8b7daa37854ab3
mciminesi <michael.ciminesi78@myhunter.cuny.edu>
2017-12-03 20:59:42 -0500

    Updated styles for links.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,8 +13,7 @@ import { AuthGuard } from './auth-guard.service';
 
 const APP_ROUTES = [
 
-  { path: '', component: HomeComponent },
-  { path: 'brand', component: AboutComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'upload', component: UploadFileComponent, canActivate: [AuthGuard] },
   { path: 'analysis', component: AnalysisComponent, canActivate: [AuthGuard] },
@@ -22,6 +21,8 @@ const APP_ROUTES = [
   // { path: 'documents/:item', component: DocumentListComponent },
   { path: 'callback', component: CallbackComponent },
   { path: '404', component: NotFoundComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'brand', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
 
